3afae9db8bf9cc953637a212dae38b1f5026fed9
Drew <sirampr@gmail.com>
2024-12-03 17:26:50 -0500

    day 2 part 2

--- 2/index.ts
+++ 2/index.ts
@@ -6,8 +6,7 @@ const decoder = new TextDecoder("utf-8");
 const fileContents = decoder.decode(data);
 const reports = fileContents.split("\n");
 
-const safeReports = reports.filter((report) => {
-  const numbers = report.split(" ").map(Number);
+function isSequenceSafe(numbers: number[]) {
   let increasing: boolean | null = null;
 
   for (let i = 1; i < numbers.length; i++) {
@@ -26,6 +25,23 @@ const safeReports = reports.filter((report) => {
   }
 
   return true;
+}
+
+const safeReports = reports.filter((report) => {
+  const numbers = report.split(" ").map(Number);
+
+  if (isSequenceSafe(numbers)) {
+    return true;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    const modifiedSequence = [...numbers.slice(0, i), ...numbers.slice(i + 1)];
+    if (isSequenceSafe(modifiedSequence)) {
+      return true;
+    }
+  }
+
+  return false;
 });
 
 console.log("Safe reports:", safeReports.length);
